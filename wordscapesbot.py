018cba335786f907aa498b4977f1744a0c59d24d
J-Zach Loke <jzloke@gmail.com>
2019-02-16 23:09:57 -0800

    added a mouse position finder and began OCR implementation

--- wordscapesbot.py
+++ wordscapesbot.py
@@ -1,36 +1,46 @@
-import pyautogui as mouse
+import pyautogui
 from itertools import permutations
 from pynput.keyboard import Key, Listener
 import threading
 import os
+import json
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
 
-COL1 = 605
-COL2 = 680
-COL3 = 755
 
-ROW1 = 425
-ROW2 = 470
-ROW3 = 560
-ROW4 = 600
+# middle
+COL1 = 175
+COL2 = 285
+COL3 = 390
+
+ROW1 = 590
+ROW2 = 650
+ROW3 = 780
+ROW4 = 840
+
+
+# image sizes
+WIDTH = 80
+HEIGHT = 80
 
 
 def move1():
-    mouse.mouseDown(x=COL2, y=ROW1)
+    pyautogui.mouseDown(x=COL2, y=ROW1)
 
 def move2():
-    mouse.mouseDown(x=COL3, y=ROW2)
+    pyautogui.mouseDown(x=COL3, y=ROW2)
 
 def move3():
-    mouse.mouseDown(x=COL3, y=ROW3)
+    pyautogui.mouseDown(x=COL3, y=ROW3)
 
 def move4():
-    mouse.mouseDown(x=COL2, y=ROW4)
+    pyautogui.mouseDown(x=COL2, y=ROW4)
 
 def move5():
-    mouse.mouseDown(x=COL1, y=ROW3)
+    pyautogui.mouseDown(x=COL1, y=ROW3)
 
 def move6():
-    mouse.mouseDown(x=COL1, y=ROW2)
+    pyautogui.mouseDown(x=COL1, y=ROW2)
 
 
 def move(pos: int):
@@ -44,35 +54,66 @@ def move(pos: int):
     do[pos]()
 
 
+
+
+
+
+
+# load a dictionary so i don't have to brute force every combo, just ones that happen to be words
+with open('words_dictionary.json') as file:
+    dictionary = [word for word in list(json.load(file).keys()) if len(word) > 2 and len(word) < 7]
+
+
+
+
+def start_level():
+    pos1 = pyautogui.screenshot('images/pos1.png', region=(COL2 - int(WIDTH / 2), ROW1 - int(HEIGHT / 2), WIDTH, HEIGHT))
+    pos2 = pyautogui.screenshot('images/pos2.png', region=(COL3 - int(WIDTH / 2), ROW2 - int(HEIGHT / 2), WIDTH, HEIGHT))
+    pos3 = pyautogui.screenshot('images/pos3.png', region=(COL3 - int(WIDTH / 2), ROW3 - int(HEIGHT / 2), WIDTH, HEIGHT))
+    pos4 = pyautogui.screenshot('images/pos4.png', region=(COL2 - int(WIDTH / 2), ROW4 - int(HEIGHT / 2), WIDTH, HEIGHT))
+    pos5 = pyautogui.screenshot('images/pos5.png', region=(COL1 - int(WIDTH / 2), ROW3 - int(HEIGHT / 2), WIDTH, HEIGHT))
+    pos6 = pyautogui.screenshot('images/pos6.png', region=(COL1 - int(WIDTH / 2), ROW2 - int(HEIGHT / 2), WIDTH, HEIGHT))
+
+
+    print(pytesseract.image_to_string(pos1, config='--psm 10'))
+    print(pytesseract.image_to_string(pos2, config='--psm 10'))
+    print(pytesseract.image_to_string(pos3, config='--psm 10'))
+    print(pytesseract.image_to_string(pos4, config='--psm 10'))
+    print(pytesseract.image_to_string(pos5, config='--psm 10'))
+    print(pytesseract.image_to_string(pos6, config='--psm 10'))
+
+
 def bruteforce():
     threes = list(permutations(range(1,7), 3))
     fours = list(permutations(range(1,7), 4))
     fives = list(permutations(range(1,7), 5))
     sixes = list(permutations(range(1,7), 6))
 
-    allcombos = threes + fours + fives + sixes
-
+    allcombos = [word for word in [threes + fours + fives + sixes] if word in dictionary]
     for combo in allcombos:
         for pos in combo:
             move(pos)
-        mouse.mouseUp()
+        pyautogui.mouseUp()
 
 
 
 
-def stop(key):
-    if key == Key.esc:
-        print('stopped')
-        os._exit(1)
+
+start_level()
+
+# def stop(key):
+#     if key == Key.esc:
+#         print('stopped')
+#         os._exit(1)
 
 
-def listen():
-    with Listener(on_press=stop) as listener:
-        listener.join()
+# def listen():
+#     with Listener(on_press=stop) as listener:
+#         listener.join()
 
 
-listen_therad = threading.Thread(target=listen)
-listen_thread.daemon = True
-listen_thread.start()
+# listen_therad = threading.Thread(target=listen)
+# listen_thread.daemon = True
+# listen_thread.start()
 
-bruteforce()
+# bruteforce()
